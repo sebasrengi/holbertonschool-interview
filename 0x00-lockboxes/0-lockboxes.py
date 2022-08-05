d0e45655d2b92bc6f69bc937b18d0c35568da3dd
sebasrengi <85517658+sebasrengi@users.noreply.github.com>
2022-08-04 21:32:28 -0500

    Update 0-lockboxes.py

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    You have n number of locked boxes in front of you. Each
+    box is numbered sequentially from 0 to n - 1 and each box
+    may contain keys to the other boxes.
+"""
+
+
+def canUnlockAll(boxes):
+    """canUnlockAll
+    Keyword arguments:
+    argument -- method which determines when all the boxes
+    could be opened
+    Return: false or true
+    """
+
+    myKeys = [0]
+
+    for key in myKeys:
+        for item in boxes[key]:
+            if item not in myKeys and item < len(boxes):
+                myKeys.append(item)
+
+    if len(myKeys) == len(boxes):
+        return True
+    return False
