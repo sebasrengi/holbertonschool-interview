01cc0fc6987d09df73cb5479753a03ceb885e9f2
sebasrengi <85517658+sebasrengi@users.noreply.github.com>
2022-12-05 04:39:43 +0100

    Update 0-starwars_characters.js

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,22 +1,29 @@
 #!/usr/bin/node
 
 const request = require('request');
-const filmId = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
 
-request(url, async (err, response, body) => {
-  if (err) {
-    console.log(err);
-  }
-  for (const characterId of JSON.parse(body).characters) {
-    await new Promise((resolve, reject) => {
-      request(characterId, (err, response, body) => {
-        if (err) {
-          reject(err);
-        }
-        console.log(JSON.parse(body).name);
-        resolve();
-      });
-    });
-  }
-});
+try {
+  const movieId = process.argv[2];
+  const url = `https://swapi.dev/api/films/${movieId}/`;
+  request(url, async function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      const characters = JSON.parse(body).characters;
+      for (const character of characters) {
+        const res = await new Promise((resolve, reject) => {
+          request(character, (err, res, html) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(JSON.parse(html).name);
+            }
+          });
+        });
+        console.log(res);
+      }
+    }
+  });
+} catch (catchErr) {
+  console.log(catchErr);
+}
