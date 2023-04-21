99d550d71d4c8af3775f263efbed48d69baa2df2
sebasrengi <sebas.rengi@hotmail.com>
2023-04-20 23:15:54 -0500

    files

---
+++ 0x21-substring/substring.h
@@ -0,0 +1,10 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <stdlib.h>
+
+int _putchar(char);
+
+int *find_substring(char const *s, char const **words, int nb_words, int *n);
+
+#endif
